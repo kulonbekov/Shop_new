bee9451ee0023f3bfd958a1e987b296ef1854b7c
Taalay <kulonbekov@gmail.com>
2022-10-28 16:00:34 +0600

    добавил в class Main все нужные структуры для работы с операциями CRUD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import com.company.db.DBEmployee;
 import com.company.db.DBProduct;
 import com.company.db.DBShop;
+import com.company.db.impl.DBEmployeeImpl;
 import com.company.db.impl.DBProductImpl;
 import com.company.db.impl.DBShopImpl;
+import com.company.models.Employee;
 import com.company.models.Product;
 import com.company.models.Shop;
 
@@ -14,12 +17,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         DBShop dbShop = new DBShopImpl();
         DBProduct dbProduct = new DBProductImpl();
+        DBEmployee dbEmployee = new DBEmployeeImpl();
 
         while(true) {
             System.out.println("Выберите таблицу из БД для изменения");
-            System.out.println("Shop 1, Product 2");
+            System.out.println("Shop 1, Product 2, Employee 3");
             boolean flag1 = true;
             boolean flag2 = true;
+            boolean flag3 = true;
             switch (sc.nextInt()) {
                 case 1:
                     while (flag1) {
@@ -81,6 +86,44 @@ public class Main {
                                 break;
                             case 5:
                                 flag2 = false;
+                                break;
+                        }
+                    }
+                case 3:
+                    while (flag3) {
+                        System.out.println("Какую операцию вы желаете провести?");
+                        System.out.println("Добавить 1,Изменить 2, Получить 3, Удалить 4, Выход 5");
+                        switch (sc.nextInt()) {
+                            case 1:
+                                System.out.println("Введите номер сотрудника");
+                                int numberEmployee = sc.nextInt();
+                                System.out.println("Введите имя сотрудника: ");
+                                String name = sc.next();
+                                System.out.println("Введите возраст сотрудника: ");
+                                int age = sc.nextInt();
+                                dbEmployee.insertEmployee(new Employee(numberEmployee, name, age));
+                                break;
+                            case 2:
+                                System.out.println("Введите id сотрудника, для изменения");
+                                long id = sc.nextInt();
+                                System.out.println("Введите новый номер сотрудника");
+                                numberEmployee = sc.nextInt();
+                                System.out.println("Введите имя нового сотрудника");
+                                name = sc.next();
+                                System.out.println("Введите возраст нового сотрудника");
+                                age = sc.nextInt();
+                                dbEmployee.updateEmployee(id,numberEmployee,name,age);
+                                break;
+                            case 3:
+                                System.out.println("Список сотрудников");
+                                System.out.println(dbEmployee.selectAllEmployee());
+                                break;
+                            case 4:
+                                System.out.println("Введите id сотрудника для удаления");
+                                dbEmployee.deleteEmployee(sc.nextLong());
+                                break;
+                            case 5:
+                                flag3 = false;
                                 break;
                         }
                     }
--- src/com/company/db/impl/DBEmployeeImpl.java
+++ src/com/company/db/impl/DBEmployeeImpl.java
@@ -1,28 +1,90 @@
 package com.company.db.impl;
 
 import com.company.db.DBEmployee;
+import com.company.db.DBGetConnection;
 import com.company.models.Employee;
+import com.company.models.Product;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBEmployeeImpl implements DBEmployee {
+
+    DBGetConnection dbGetConnection = new DBGetConnectionImpl();
     @Override
     public void insertEmployee(Employee employee) {
+        try {
+            PreparedStatement ps = dbGetConnection.getConnection("Insert into tb_employee(numberEmployee, name, age) Values (?,?,?)");
+            ps.setInt(1,employee.getNumberEmployee());
+            ps.setString(2, employee.getName());
+            ps.setInt(3, employee.getAge());
 
+            ps.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throw  new RuntimeException("Произошла ошибка при сохранении преподавателя");
+        }
     }
 
     @Override
     public List<Employee> selectAllEmployee() {
-        return null;
+
+        try {
+            PreparedStatement ps = dbGetConnection.getConnection("select * from tb_employee");
+            ResultSet resultSet = ps.executeQuery();
+            List<Employee> employees=new ArrayList<>();
+            while (resultSet.next()){
+                Employee employee = new Employee();
+                employee.setId(resultSet.getLong("id"));
+                employee.setNumberEmployee(resultSet.getInt("numberEmployee"));
+                employee.setName(resultSet.getString("name"));
+                employee.setAge(resultSet.getInt("age"));
+                employees.add(employee);
+            }
+            return employees;
+
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Произошла ошибка при выводе списка");
+        }
     }
 
     @Override
     public void updateEmployee(Long id, int numberEmployee, String name, int age) {
 
+        try {
+            PreparedStatement ps = dbGetConnection.getConnection("Update tb_employee set numberEmployee = ? name =?, age = ? where id =?");
+            ps.setInt(1, numberEmployee);
+            ps.setString(2, name);
+            ps.setInt(3, age);
+            ps.setInt(4, Math.toIntExact(id));
+            ps.executeUpdate();
+
+        }  catch (SQLException throwables) {
+            throw new RuntimeException("Произошла ошибка при изменения данных преподавателя");
+        }
     }
 
     @Override
     public void deleteEmployee(Long id) {
+
+        try {
+            PreparedStatement ps = dbGetConnection.getConnection("Delete from tb_employee where id = ?");
+            ps.setInt(1, Math.toIntExact(id));
+            int result = ps.executeUpdate();
+            System.out.println(result);
+
+            if(result==1){
+                System.out.println("Объект успешно удален");
+            }else if(result==0){
+                System.out.println(" Запрос успешно выполнен. Заняло 0мс, 0 строк изменено");
+            }
+
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Произошла ошибка при удалении ");
+        }
 
     }
 }
