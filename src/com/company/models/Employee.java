f27ff89a767d0d7ea5abd06a67a11bb7fe42d05f
Taalay <kulonbekov@gmail.com>
2022-10-28 15:31:42 +0600

    new class Employee (конструктор, геттер и сеттер, toString)

--- src/com/company/models/Employee.java
+++ src/com/company/models/Employee.java
@@ -1,4 +1,59 @@
 package com.company.models;
 
-public class Employee {
+public class Employee{
+    private Long id;
+    private int numberEmployee;
+    private String name;
+    private int age;
+
+    public Employee(int numberEmployee, String name, int age) {
+        this.numberEmployee = numberEmployee;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumberEmployee() {
+        return numberEmployee;
+    }
+
+    public void setNumberEmployee(int numberEmployee) {
+        this.numberEmployee = numberEmployee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", numberEmployee=" + numberEmployee +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
